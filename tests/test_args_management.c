da684ea20aadbf9aeebf3184f42ed8ecf6948eef
osvegn <thomas.prudhomme@epitech.eu>
2022-04-12 23:47:06 +0200

    [ADD] update test error message on error output

--- tests/test_args_management.c
+++ tests/test_args_management.c
@@ -20,14 +20,14 @@ Test(args_management, valide_arguments)
     cr_assert_eq(rvalue, 0);
 }
 
-Test(args_management, invalide_arguments_numbers, .init=cr_redirect_stdout)
+Test(args_management, invalide_arguments_numbers, .init=cr_redirect_stderr)
 {
     int ac = 3;
     char *av[5] = {"./panoramix", "1", "2", "3", "4"};
     int numbers[ARGS_NUMBER];
 
     args_management(ac, av, numbers);
-    cr_assert_stdout_eq_str("USAGE: ./panoramix <nb_villagers> <pot_size> <nb_fights> <nb_refills>\n");
+    cr_assert_stderr_eq_str("USAGE: ./panoramix <nb_villagers> <pot_size> <nb_fights> <nb_refills>\n");
 }
 
 Test(args_management, invalide_arguments)
