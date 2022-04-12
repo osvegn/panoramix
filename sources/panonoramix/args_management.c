a47bdf6a6f69a3f72bf85635e3384dd32d0249ed
osvegn <thomas.prudhomme@epitech.eu>
2022-04-12 23:46:02 +0200

    [FIX] error message if an argument is invalid

--- sources/panonoramix/args_management.c
+++ sources/panonoramix/args_management.c
@@ -11,14 +11,15 @@
 
 /**
 * @brief It checks if the number of arguments is correct, and if so, it
-* converts the arguments to integers and stores them in the array
+* converts them to
+* integers and stores them in the array pointed to by `number`
 *
 * @param ac the number of arguments passed to the program
 * @param av the array of arguments
-* @param number the array of integers that will be filled with the values of
-* the arguments
+* @param number the array of integers that will be filled with the arguments
 *
-* @return 0 if the arguments are valid, -1 if they are not.
+* @return 0 if the arguments are valid, 1 if the number of
+* arguments is invalid and -1 if the arguments are not valid.
 */
 int args_management(int ac, char **av, int *number)
 {
@@ -28,13 +29,12 @@ int args_management(int ac, char **av, int *number)
         return (-1);
     if (ac != ARGS_NUMBER + 1) {
         print_usage(av[0]);
-        return (0);
+        return (1);
     }
     for (int i = 0; i < 4; i++) {
         rvalue = atoi(av[i + 1]);
         if (rvalue <= 0) {
-            fprintf(stderr, "Error: args_management: value \"%s\" is not a \
-valide argument\n", av[i + 1]);
+            fprintf(stderr, "Values must be an integer >0.\n");
             return (-1);
         }
         number[i] = rvalue;
