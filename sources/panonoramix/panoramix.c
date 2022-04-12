1fc57c94f2f21329068394c954b2151258b86c87
osvegn <thomas.prudhomme@epitech.eu>
2022-04-13 00:18:25 +0200

    [ADD] a forget file

--- sources/panonoramix/panoramix.c
+++ sources/panonoramix/panoramix.c
@@ -6,49 +6,6 @@
 */
 
 #include "panoramix.h"
-#include <stdio.h>
-#include <pthread.h>
-#include <stdlib.h>
-
-void *druid(void *data)
-{
-    printf("Druid: I'm ready... but sleepy...\n");
-    fflush(stdout);
-    (void)data;
-    return (NULL);
-}
-
-void *villager(void *d)
-{
-    data_t *data = d;
-
-    printf("Villager %i: Going into battle!\n", data->id);
-    fflush(stdout);
-    (void)data;
-    return (NULL);
-}
-
-int run_panoramix(int *numbers)
-{
-    pthread_t *villagers = NULL;
-    data_t *data = NULL;
-
-    villagers = malloc(sizeof(pthread_t) * (numbers[NB_VILLAGERS] + 1));
-    data = malloc(sizeof(data_t) * (numbers[NB_VILLAGERS] + 1));
-    if (!villagers || !data) {
-        fprintf(stderr, "Error: run_panoramix: malloc failed\n");
-        return (-1);
-    }
-    pthread_create(&villagers[0], NULL, &druid, NULL);
-    for (int i = 1; i < numbers[NB_VILLAGERS] + 1; i++) {
-        data[i].id = i - 1;
-        pthread_create(&villagers[i], NULL, &villager, &data[i]);
-    }
-    for (int i = 1; i < numbers[NB_VILLAGERS] + 1; i++) {
-        pthread_join(villagers[i], NULL);
-    }
-    return (0);
-}
 
 int panoramix(int ac, char **av)
 {
