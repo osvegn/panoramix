43ba87127d3c4b454d1c5119fa819b6e90ac8914
osvegn <thomas.prudhomme@epitech.eu>
2022-04-12 23:45:22 +0200

    [ADD] enum args_type and struct data_t

--- includes/panoramix.h
+++ includes/panoramix.h
@@ -12,6 +12,17 @@
 
 #define ARGS_NUMBER 4
 
+typedef enum args_type_e {
+    NB_VILLAGERS,
+    POT_SIZE,
+    NB_FIGHTS,
+    NB_REFILLS
+} args_type_t;
+
+typedef struct data_s {
+    int id;
+} data_t;
+
 void print_usage(char *name);
 int args_management(int ac, char **av, int *number);
 int panoramix(int ac, char **av);
