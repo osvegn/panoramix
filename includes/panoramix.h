fbb83263eaeca69998e380e1b3b969a00181b205
osvegn <thomas.prudhomme@epitech.eu>
2022-04-14 15:04:46 +0200

    [ADD] villager sentance type enum

--- includes/panoramix.h
+++ includes/panoramix.h
@@ -12,6 +12,14 @@
 
 #define ARGS_NUMBER 4
 
+typedef enum villager_sentance_type_e {
+    VILLAGER_START,
+    VILLAGER_DRINK,
+    VILLAGER_REFILL,
+    VILLAGER_FIGHT,
+    VILLAGER_DONE,
+} villager_sentance_type_t;
+
 typedef enum args_type_e {
     NB_VILLAGERS,
     POT_SIZE,
@@ -29,3 +37,4 @@ int panoramix(int ac, char **av);
 int run_panoramix(int *numbers);
 void *villager(void *d);
 void *druid(void *data);
+void print_villager_sentance(villager_sentance_type_t type, int id, int value);
