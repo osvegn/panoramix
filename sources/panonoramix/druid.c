af16e22b154ce59a4fc2cde338df83f073654f07
osvegn <thomas.prudhomme@epitech.eu>
2022-04-20 21:38:12 +0200

    [NEW] druid can print sentances and do his work

--- sources/panonoramix/druid.c
+++ sources/panonoramix/druid.c
@@ -8,10 +8,35 @@
 #include "panoramix.h"
 #include <stdio.h>
 
-void *druid(void *data)
+void print_druid_sentance(druid_sentance_type_t type, int value)
 {
-    printf("Druid: I'm ready... but sleepy...\n");
+    if (type == DRUID_START)
+        printf("Druid: I'm ready... but sleepy...\n");
+    if (type == DRUID_REFILL)
+        printf("Druid: Ah! Yes, yes, I'm awake! Working on it! Beware I can only make %i more refills after this one.\n", value);
+    if (type == DRUID_SLEEP)
+        printf("Druid: I'm out of viscum. I'm going back to... zZz\n");
     fflush(stdout);
-    (void)data;
+}
+
+void *druid(void *d)
+{
+    villagers_data_t *data = d;
+
+    print_druid_sentance(DRUID_START, 0);
+    while (data->numbers[NB_REFILLS] > 0) {
+        sem_wait(data->sem);
+        if (data->villagers_status == true) {
+            data->numbers[NB_REFILLS]--;
+            (*data->nb_pots) = data->numbers[POT_SIZE];
+            print_druid_sentance(DRUID_REFILL, data->numbers[NB_REFILLS]);
+            sem_post(data->sem2);
+        } else {
+            print_druid_sentance(DRUID_SLEEP, 0);
+            sem_post(data->sem2);
+            return (NULL);
+        }
+    }
+    print_druid_sentance(DRUID_SLEEP, 0);
     return (NULL);
 }
